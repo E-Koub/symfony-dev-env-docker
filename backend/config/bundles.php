9cb67352bd0c41e3f36b6aa348304e5b4e9df73b
eudes <eudes.koub@gmail.com>
2025-05-23 10:36:59 +0200

    feat(docker): add npm and yarn support in PHP Symfony container

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -2,4 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Svelte\SvelteBundle::class => ['all' => true],
 ];
